860bb7485d108363487677167c8de9f1497cf46e
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2021-09-05 15:59:09 +0200

    update

--- src/bin/index.js
+++ src/bin/index.js
@@ -3,12 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+import routes from '../config/routes'
 import {sendSuccessResponse} from '../app/helpers/responses.helpers.js';
 import {successCodes} from '../app/helpers/statusCodes.helper.js';
 import {successMessages} from '../app/helpers/messages.helpers.js';
 
+
 const {ok} = successCodes;
 const {welcome} = successMessages;
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
+app.use("/api", routes);
 
 app.get('/', (req,res)=>{
     sendSuccessResponse(res, ok, welcome,null, null)
