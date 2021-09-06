f9e2d58e6ab802b13d829224ee67690b243a1f76
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2021-09-06 15:46:09 +0200

    update

--- src/config/routes/api/class.routes.js
+++ src/config/routes/api/class.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import ctrl from '../../../app/controllers/class.controller';
+import validate from '../../validations/class.validation';
 
 const route = express.Router();
 
 route
-    .post('/create', ctrl.register)
+    .post('/create', validate.register, ctrl.register)
 
 
 export default route;
