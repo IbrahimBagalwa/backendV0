9ba84d75ed7785a97cb70ad91182d3cb2fc0f8d0
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-26 11:05:43 +0200

    update

---
+++ src/db/migrations/20210926090525-create-option.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Options', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      idSection: {
+        type: Sequelize.INTEGER
+      },
+      datastatus: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Options');
+  }
+};
