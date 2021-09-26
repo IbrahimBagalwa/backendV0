64c18c66544fe13e7524f07d7d0f84d8cda35fc5
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-26 11:03:30 +0200

    update

---
+++ src/db/migrations/20210926090300-create-section.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sections', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      datastatus: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sections');
+  }
+};
