2409534db549eeed02d81d6be9fec336a03a90d3
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-29 08:00:49 +0200

    validation user

--- src/config/routes/api/student.routes.js
+++ src/config/routes/api/student.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import ctrl from '../../../app/controllers/students.controller';
 import { checkIsAdmin, checkToken } from '../../middlewares/auth.user';
+import val from '../../validations/student.validation';
 const route = express.Router()
 
 route
     .get('/current-user', checkToken, ctrl.getCurrent)
     .get('/view-all',checkToken, checkIsAdmin, ctrl.view)
     .get('/view-id/:id', ctrl.viewById)
-    .post('/register-student', ctrl.register)
+    .post('/register-student', val.register, ctrl.register)
     .post('/login', ctrl.login)
     .put('/update-student',checkIsAdmin,checkToken, ctrl.update)
